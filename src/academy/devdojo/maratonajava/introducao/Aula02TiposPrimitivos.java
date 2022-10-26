96ff212e0276fb98edc5f7244ef7a044faeaed72
wendelsimoes <wendel.simoes.br@gmail.com>
2022-10-25 22:23:44 -0300

    3 - Tipos primitivos pt 04 - Strings

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,16 +3,23 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long) 100000.253;
         double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        float salarioFloat = 2500.0F;
         byte idadeByte = 10;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char charactere = 'M';
+
+        // string
+        String nome = "Goku";
+
         System.out.println("A idade é " + idade + " anos");
         System.out.println(verdadeiro);
+        System.out.println(idade);
+        System.out.println(numeroGrande);
+        System.out.println(nome);
     }
 }
