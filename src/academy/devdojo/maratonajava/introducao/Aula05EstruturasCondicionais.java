5fbd809036e8f926d5de41dd75f2cf3778fcab51
wendelsimoes <wendel.simoes.br@gmail.com>
2022-10-26 17:01:34 -0300

    20 - Estruturas Condicionais pt 01 - IF

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 17;
+
+        if (idade >= 18) {
+            System.out.println("Autorizado");
+        }
+        else {
+            System.out.println("Não autorizado");
+        }
+    }
+}
