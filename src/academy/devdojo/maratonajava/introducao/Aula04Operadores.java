e0bcdf2d6ba415444f88c66fadc9f170d1a3df94
wendelsimoes <wendel.simoes.br@gmail.com>
2022-10-25 23:06:43 -0300

    15 - Operadores pt 01 - Aritiméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println(numero2 - numero1);
+        System.out.println(numero2 + numero1);
+        System.out.println("Valor " + numero2 + numero1);
+        System.out.println(numero2 + numero1 + " Valor");
+        System.out.println(numero2 / numero1);
+        System.out.println(numero1 / numero2);
+        System.out.println((double) numero1 / numero2);
+    }
+}
